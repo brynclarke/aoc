2da47bc0bf083a9b9dea4f56354ca5bad337b76a
brynclarke <clarke.bryn@gmail.com>
2022-12-08 01:08:29 -0600

    cleaner numpy solution

--- aoc/solutions/year2022/day08.py
+++ aoc/solutions/year2022/day08.py
@@ -1,73 +1,39 @@
+import numpy as np
+
 def solve(input_text):
-    w = input_text.index("\n")
-    h = int(len(input_text) / (w + 1))
-
     part1 = 0
-    m = {}
+    part2 = 0
 
-    for i in range(h): # row
-        for j in range(w): # col
-            m[(i, j)] = [0, 0, 0, 0]
-            t1 = input_text[i*(w+1) + j]
+    arr = np.array([[int(x) for x in line] for line in input_text.split()])
 
-            vis_left = True
-            for k in range(j-1, -1, -1):
-                t2 = input_text[i*(w+1) + k]
+    for (tr, tc), tree_height in np.ndenumerate(arr):
+        trees_left = arr[tr, :tc][::-1]
+        trees_right = arr[tr, tc+1:]
+        trees_above = arr[:tr, tc][::-1]
+        trees_below = arr[tr+1:, tc]
 
-                # tree left
-                m[(i, j)][0] += 1
+        tree_visible = \
+            np.all(trees_left < tree_height) or \
+            np.all(trees_right < tree_height) or \
+            np.all(trees_above < tree_height) or \
+            np.all(trees_below < tree_height)
+        
+        def viewing_distance(trees):
+            trees_in_view = trees[:np.argmax(np.hstack([
+                False,
+                trees >= tree_height,
+                True
+            ]))]
 
-                if int(t2) >= int(t1):
-                    vis_left = False
-                    # not vis left
-                    break
+            return len(trees_in_view)
 
-            vis_right = True
-            for k in range(j+1, w):
-                t2 = input_text[i*(w+1) + k]
+        scenic_score = np.prod([
+            viewing_distance(trees) for trees in [
+                trees_left, trees_right, trees_above, trees_below
+            ]
+        ])
 
-                # tree right
-                m[(i, j)][1] += 1
-
-                if int(t2) >= int(t1):
-                    vis_right = False
-                    # not vis right
-                    break
-
-            vis_top = True
-            for k in range(i-1, -1, -1):
-                t2 = input_text[k*(w+1) + j]
-
-                # tree top
-                m[(i, j)][2] += 1
-
-                if int(t2) >= int(t1):
-                    vis_top = False
-                    # not vis top
-                    break
-
-            vis_bot = True
-            for k in range(i+1, h):
-                t2 = input_text[k*(w+1) + j]
-
-                # tree bot
-                m[(i, j)][3] += 1
-
-                if int(t2) >= int(t1):
-                    vis_bot = False
-                    # not vis bot
-                    break
-
-            if vis_left or vis_right or vis_top or vis_bot:
-                part1 += 1
-
-    scenic_scores = []
-    for dir_score in m.values():
-        score = 1
-        for s in dir_score:
-            score *= s
-        scenic_scores += [score]    
-
-    part2 = max(scenic_scores)
+        if tree_visible: part1 += 1
+        part2 = max(part2, scenic_score)
 
     return (part1, part2)
