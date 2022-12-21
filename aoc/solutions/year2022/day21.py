df05c28b99603560e743b3ee9a1b35c04a1560b0
brynclarke <clarke.bryn@gmail.com>
2022-12-21 00:13:48 -0600

    bit of a breather day

---
+++ aoc/solutions/year2022/day21.py
@@ -0,0 +1,60 @@
+def solve(input_text):
+    commands = {}
+    for line in input_text.strip().splitlines():
+        commands[line[:4]] = line[6:].split(" ")
+
+    def evaluate(key, humn=None):
+        op = commands[key]
+
+        if key == "humn" and humn is not None:
+            return humn
+
+        if len(op) == 1:
+            return int(op[0])
+        
+        assert op[0] in commands and op[2] in commands
+
+        lhs = evaluate(op[0], humn)
+        rhs = evaluate(op[2], humn)
+
+        if key == "root" and humn is None: return int(lhs + rhs) # part1
+        if key == "root" and humn is not None: return lhs - rhs # part2
+        if op[1] == "+": return lhs + rhs
+        elif op[1] == "-": return lhs - rhs
+        elif op[1] == "*": return lhs * rhs
+        elif op[1] == "/": return lhs / rhs
+        else: assert False
+
+    # part1 is a simple recursive evaluation
+    part1 = evaluate("root")
+
+    # part 2 requires trial and error
+    # we track the prior guess and make educated next guesses
+    prior_h = None
+    prior_r = None
+
+    h = 1
+    while True:
+        r = evaluate("root", h)
+
+        if r == 0:
+            break
+
+        # use a simple numerican method to find the next guess
+        # (I think this is called Euler's technique? extend x in the direction 
+        # and by the amount the slope indicates is needed to reach zero)
+        if prior_h is None:
+            h_est = h + 1 # basically seeding the slope
+        else:
+            slope = (r - prior_r) / (h - prior_h)
+            # but also (assuming slope is constant)
+            # slope = (goal - r) / (h_est - h)
+            h_est = h + (0 - r) / slope
+
+        prior_h, prior_r = h, r
+        h = h_est
+
+    # converged to solution
+    part2 = int(h)
+
+    return part1, part2
---
+++ aoc/tests/year2022/day21.py
@@ -0,0 +1,25 @@
+from aoc.solutions.year2022.day21 import solve
+
+def unit_test():
+    input_text = """
+root: pppw + sjmn
+dbpl: 5
+cczh: sllz + lgvd
+zczc: 2
+ptdq: humn - dvpt
+dvpt: 3
+lfqf: 4
+humn: 5
+ljgn: 2
+sjmn: drzm * dbpl
+sllz: 4
+pppw: cczh / lfqf
+lgvd: ljgn * ptdq
+drzm: hmdt - zczc
+hmdt: 32
+"""
+
+    solution = solve(input_text)
+    expected_solution = (152, 301)
+
+    assert solution == expected_solution
